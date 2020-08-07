fa1e50e63ebbcb4a295aa6ce93924660426b5ae4
krishnaglodha <47075664+krishnaglodha@users.noreply.github.com>
2020-08-07 17:53:21 +0530

    API added

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,5 +1,16 @@
-$.getJSON('https://testapi.sixwallz.com/api/v1/neighborhood/', {
-    neighborhoodId: "150932"    
- }, function(data){
-    console.log(data)
+
+$.ajax({
+    url:'https://testapi.sixwallz.com/api/v1/neighborhood/',
+    headers:{'Authorization':'Token eb02d455aabbd1c420015a7638e8b522a83dea6f'},
+    data:{'neighborhoodId': "150932" },
+    type:'GET',
+    dataType: 'json',
+    contentType: 'application/json; charset=utf-8',
+    success: function (result) {
+        // CallBack(result);
+        console.log(result)
+     },
+            error: function (error) {
+                    console.log(error)
+            }
 })
